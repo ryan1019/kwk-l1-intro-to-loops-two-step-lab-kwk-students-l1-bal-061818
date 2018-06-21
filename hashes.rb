e676883e3d00b4b13949c5dc1bf88bbdaa3133c2
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-21 14:27:28 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,1 +1,2 @@
-groceries_hash = {"apple" => 1.99, "avocado" => 0.99,"pasta" => 9.99, ""}
+groceries_hash = {"apple" => 1.99, "avocado" => 0.99,"pasta" => 9.99, "bananas" => 3.99}
+puts groceries_hash.keys
